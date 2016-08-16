509ea341b08491c402003147c1ca4667dd90d601
Jagureo <jagnzet@gmail.com>
2016-08-16 17:08:29 +0800

    enemy dll

--- SP3Framework/enemy/enemy.cpp
+++ SP3Framework/enemy/enemy.cpp
@@ -3,6 +3,7 @@
 enemy::enemy(Vector3 pos)
 {
 	position = pos;
+	newSpawn = 1;
 }
 
 enemy::~enemy()
@@ -15,9 +16,39 @@ Vector3 enemy::getPos()
 	return position;
 }
 
-void enemy::setPos(Vector3 pos)
+bool enemy::getNewSpawn()
 {
-	position = pos;
+	return newSpawn;
+}
+
+void enemy::setNewSpawn(bool input)
+{
+	newSpawn = input;
+}
+
+void enemy::setPos(float x, float y, float z)
+{
+	position = Vector3(x,y,z);
+}
+
+Vector3 enemy::getVel()
+{
+	return vel;
+}
+
+void enemy::setVel(float x, float y, float z)
+{
+	vel.Set(x, y, z);
+}
+
+void enemy::runOff(Vector3 playerPos)
+{
+	vel = -(playerPos - position) * 3;
+}
+
+void enemy::updatePos(float dt)
+{
+	position += vel * dt * .2;
 }
 
 enemy* newEnemy(float x, float y, float z)
--- SP3Framework/enemy/enemy.h
+++ SP3Framework/enemy/enemy.h
@@ -12,12 +12,27 @@
 	class ENEMY_API enemy
 	{
 	public:
+		
 		enemy(Vector3 pos);
 		~enemy();
 		Vector3 getPos();
-		void setPos(Vector3 pos);
+		Vector3 getVel();
+		bool getNewSpawn();
+		void setNewSpawn(bool input);
+		void runOff(Vector3 player);
+		void setVel(float x, float y, float z);
+		void setPos(float x, float y, float z);
+		void updatePos(float dt);
+		enum ENEMY_TYPE
+		{
+			EN_NONE = 0,
+			EN_ZEBRA,
 
+			EN_TOTAL, //must be last
+		};
 		Vector3 position;
+		Vector3 vel;
+		bool newSpawn;
 	};
 
 	ENEMY_API enemy* newEnemy(float x, float y, float z);
